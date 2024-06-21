6569196df6121a6c2d93f86d2c949f17fa5f4418
Abhi123-9693 <rajabhishek8867@gmail.com>
2024-06-21 15:15:59 +0530

    add a file

--- gsap.js
+++ gsap.js
@@ -1,5 +1,22 @@
-gsap .from("#abhi",{
-  fontSize:"50px"  
-}
-
-)
+gsap.from("#t1",{
+  x:-500,
+  opacity:0,
+  duration:1.5,
+  scrollTrigger:{
+    trigger:"#t1",
+    markers:true,
+    scrub:true,
+    end:"top 50%"
+  }
+})
+gsap.from("#t2",{
+  x:+500,
+  opacity:0,
+  duration:1.5,
+  scrollTrigger:{
+    trigger:"#t2",
+    markers:true,
+    scrub:true,
+    end:"top 50%"
+  }
+})
